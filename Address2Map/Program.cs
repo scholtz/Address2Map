63d78c38ec5b5ee9e99a7cfe7e6d3e89ea35fc1a
Ludovit Scholtz <ludko2@gmail.com>
2022-10-08 00:19:52 +0200

    allow swagger

--- Address2Map/Program.cs
+++ Address2Map/Program.cs
@@ -39,18 +39,9 @@ builder.Services.AddSwaggerGen(c =>
 });
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
---
+++ Address2Map/build.sh
@@ -0,0 +1,3 @@
+﻿ver=1.0.0-stable
+docker build -t scholtz2/address2map:1.0.0-stable -f Dockerfile --progress=plain ..
+docker push scholtz2/address2map:1.0.0-stable
